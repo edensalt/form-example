984b5eeda5d5f068314a51a86889662495bba55c
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-30 11:03:45 -0500

    Remove error message if new input is valid

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ const inputFields = document.querySelectorAll('input');
 
 // Add an event listener to the form for the submit event
 submitButton.addEventListener('click', (e) => {
+
     // Loop through each input field and check if it's valid
     inputFields.forEach((input) => {
-        if (!input.checkValidity()) {
+
+        // Remove error messages if they exist and the new input is valid
+
+        if (input.checkValidity()) {
+            const existingErrorMessage = input.nextElementSibling;
+            if (existingErrorMessage && existingErrorMessage.classList.contains('error-message')) {
+                existingErrorMessage.remove(); // remove the existing error message
+            }
+        }
+
+        else if (!input.checkValidity()) {
             // If the input is not valid, add the error class to the input field
             input.classList.add('error');
             // Display a message explaining the error
             const errorMessage = document.createElement('div');
+            errorMessage.classList.add('error-message');
             errorMessage.innerText = input.validationMessage;
             // Style the error message
             errorMessage.style.fontSize = '0.7rem';
@@ -19,6 +31,13 @@ submitButton.addEventListener('click', (e) => {
             errorMessage.style.marginTop = '-30px';
             errorMessage.style.marginBottom = '5px';
             errorMessage.style.marginRight = '0px';
+
+            // Remove previous messages
+
+            const existingErrorMessage = input.nextElementSibling;
+            if (existingErrorMessage && existingErrorMessage.classList.contains('error-message')) {
+                existingErrorMessage.remove(); // remove the existing error message
+            }
 
             // Insert the message
             input.insertAdjacentElement('afterend', errorMessage);
